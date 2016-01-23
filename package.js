ae3f6f2507699bb23ceba0c572bfbc660bb5b8d7
sanjivj <sanjivchristopher@gmail.com>
2016-01-23 23:45:24 +0000

    Removed jquery.js and modernizr.js from "vendor" folder. Replaced these with the equivalent meteor plugins; modified package.js accordingly.

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-	name: "ewall:foundation",
+	name: "scj:foundation",
 	summary: "Foundation by ZURB - an advanced responsive web framework",
 	version: "5.5.3",
 	git: "https://github.com/ewall/meteor-foundation"
@@ -8,12 +8,12 @@ Package.describe({
 Package.on_use(function (api) {
   api.versionsFrom("METEOR@0.9.0");
 	api.use('jquery', 'client');
+	api.use('cwaring:modernizr', 'client');
 
 	var path = Npm.require('path');
 	api.add_files(path.join('css', 'normalize.css'), 'client');
 	api.add_files(path.join('css', 'foundation.min.css'), 'client');
 
-	api.add_files(path.join('js', 'vendor', 'modernizr.js'), 'client');
 	api.add_files(path.join('js', 'vendor', 'fastclick.js'), 'client');
 	api.add_files(path.join('js', 'vendor', 'jquery.cookie.js'), 'client');
 	api.add_files(path.join('js', 'vendor', 'placeholder.js'), 'client');
